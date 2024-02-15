2cbb031724dc25f023aa704c95a7cac604e07996
sampad <sampadrb@gmail.com>
2024-02-16 00:02:43 +0530

    login updates

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,67 +1,55 @@
-export async function login(authDetail)
-{
-   const requestOptions ={
-          method: "POST",
-          headers: {"content-Type": "application/json"},
-          body: JSON.stringify(authDetail)
+export async function login(authDetail) {
+  const requestOptions = {
+    method: "POST",
+    headers: { "content-Type": "application/json" },
+    body: JSON.stringify(authDetail),
+  };
 
-        }
+  const response = await fetch(
+    `${process.env.REACT_APP_HOST}/login`,
+    requestOptions
+  );
+  if (!response.ok) {
+    throw { message: response.statusText, status: response.status }; //eslint-disable-line
+  }
 
-        const response = await fetch(`${process.env.REACT_APP_HOST}/login`,requestOptions);
-         if(!response.ok)
-         {
-           throw { message: response.statusText, status: response.status }; //eslint-disable-line
-         }
-        
-        const data= await response.json();
-         //data.accessToken ? navigate("/products"):toast.error(data)
+  const data = await response.json();
+  //data.accessToken ? navigate("/products"):toast.error(data)
 
+  if (data.accessToken) {
+    sessionStorage.setItem("token", JSON.stringify(data.accessToken));
+    sessionStorage.setItem("cbid", JSON.stringify(data.user.id));
+  }
 
-         if(data.accessToken)
-         {
-            sessionStorage.setItem("token",JSON.stringify(data.accessToken))
-            sessionStorage.setItem("cbid",JSON.stringify(data.user.id))
-         }
+  return data;
+}
 
-         return data;
+export async function register(authDetail) {
+  const requestOptions = {
+    method: "POST",
+    headers: { "content-Type": "application/json" },
+    body: JSON.stringify(authDetail),
+  };
 
-        }
+  const response = await fetch(
+    `${process.env.REACT_APP_HOST}/register`,
+    requestOptions
+  );
 
+  if (!response.ok) {
+    throw { message: response.statusText, status: response.status }; //eslint-disable-line
+  }
 
+  const data = await response.json();
+  if (data.accessToken) {
+    sessionStorage.setItem("token", JSON.stringify(data.accessToken));
+    sessionStorage.setItem("cbid", JSON.stringify(data.user.id));
+  }
 
+  return data;
+}
 
-    export async function register(authDetail)
-    {
-        const requestOptions ={
-          method: "POST",
-          headers: {"content-Type": "application/json"},
-          body: JSON.stringify(authDetail)
-
-        }
-
-       const response = await fetch(`${process.env.REACT_APP_HOST}/register`,
-                        requestOptions);
-
-
-
-        if(!response.ok)
-         {
-           throw { message: response.statusText, status: response.status }; //eslint-disable-line
-         }
-      
-       const data=await response.json();
-       if(data.accessToken)
-         {
-            sessionStorage.setItem("token",JSON.stringify(data.accessToken))
-            sessionStorage.setItem("cbid",JSON.stringify(data.user.id))
-         }
-
-         return data;
-    }
-
-
-    export function logout()
-    {
-         sessionStorage.removeItem("token");
-         sessionStorage.removeItem("cbid");
-    }
+export function logout() {
+  sessionStorage.removeItem("token");
+  sessionStorage.removeItem("cbid");
+}
